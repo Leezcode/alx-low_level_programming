4ad5f7c46f858715192478c6455ade67c577007c
Leezcode <liznyandia@gmail.com>
2022-09-08 23:43:09 +0300

    Printing in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - displays exactly "Programming is like building a multilingual puzzle
+ * Return: 0 if program succeeds, non-zero if it fails
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
