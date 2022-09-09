c316a8ede68bf9aa3a3f8a2d90f64603cb8e0e42
Leezcode <liznyandia@gmail.com>
2022-09-09 21:35:53 +0300

    printing numbers and alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - displays positive numbers less than 10
+ * main - displays alphabet in lowercase
  * Return: 0 if programn succeeds, non-zero if not
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 'z'; i >= 'a'; i--)
 	{
 		putchar(i);
 	}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - displays positive numbers less than 10
+ * main - displays all combinations of single-digit numbers
  * Return: 0 if programn succeeds, non-zero if not
  */
 int main(void)
@@ -11,6 +11,11 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');			
+		}
 	}
 	putchar('\n');
 	return (0);
