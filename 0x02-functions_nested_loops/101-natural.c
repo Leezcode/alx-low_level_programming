cbc282c24d0e49bb01f4f9ac081b039e6230cff4
Leezcode <liznyandia@gmail.com>
2022-09-15 08:43:18 +0300

    natural numbers code

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int i;
-	
-	for (i = 0; i < 1024; i++){
-		if ((i % 3 == 0) || (i % 5 == 0)){
-			printf("%d, ", i);	
-		}
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			printf("%d, ", i);
 	}
 	printf("\n");
 	return (0);
